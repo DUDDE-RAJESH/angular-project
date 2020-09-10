01ea10dbec0fc93d2ad05d4467c9f7ab44f09c96
DUDDE-RAJESH <rishirajesh89@gmail.com>
2020-09-10 12:11:43 +0530

    2 files modified, 5 added

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,12 @@
+import {Hero} from './hero';
+export const HEROES: Hero[]=[
+  { id:0 ,name:'rajesh(me)' },
+  { id:1 ,name:'sreenath' },
+  { id:2 ,name:'mahesh' },
+  { id:3 ,name:'raju' },
+  { id:4 ,name:'venky' },
+  { id:5 ,name:'rakesh' },
+  { id:6 ,name:'yesh' },
+  { id:7 ,name:'ajay' },
+  { id:8 ,name:'vishnu' }
+];
